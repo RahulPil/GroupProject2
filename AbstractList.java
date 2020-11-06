edad462d7b5995fd49e7953a4c0a2ccab4ca7cb3
MangoPicante <8892151+MangoPicante@users.noreply.github.com>
2020-11-06 11:30:33 -0800

    set up abstract class skeleton

--- AbstractList.java
+++ AbstractList.java
@@ -1,99 +1,51 @@
 import java.util.*;
 
-public class AbstractList<E> implements List<E> {
-    private E[] elementData; // list of values
+public abstract class AbstractList<E> implements List<E> {
     private int size; // current number of elements in the list
 
-    public static final int DEFAULT_CAPACITY = 100;
-
-    private ListNode<E> front; // first value in the list
-    private ListNode<E> back; // last value in the list
-    private int size; // current number of elements
-
+    //returns num of elements in list
     public int size() {
         return size;
     }
 
-    public E get(int index) {
-        checkIndex(index);
-        return elementData[index];
-    }
+    //returns value at given index in list
+    public abstract E get(int index);
 
-    public String toString() {
-        if (size == 0) {
-            return "[]";
-        } else {
-            String result = "[" + elementData[0];
-            for (int i = 1; i < size; i++) {
-                result += ", " + elementData[i];
-            }
-            result += "]";
-            return result;
-        }
-    }
+    //verifies that the index is in bounds of the list
+    public abstract void checkIndex(int index);
 
-    public int indexOf(E value) {
+    //returns a string representation of the list
+    public abstract String toString();
 
-    }
+    //returns the index of the first occurrence of the given value
+    //returns -1 if not found
+    public abstract int indexOf(E value);
 
-    public boolean isEmpty() {
-        return size == 0;
-    }
+    //returns true if the list is empty
+    public abstract boolean isEmpty();
 
-    public boolean contains(E value) {
-        return indexOf(value) >= 0;
-    }
+    //returns true if the list contains the value
+    public abstract boolean contains(E value);
 
-    public void add(E value) {
-        ensureCapacity(size + 1);
-        elementData[size] = value;
-        size++;
-    }
+    //appends the value to the end of the list
+    public abstract void add(E value);
 
-    public void add(int index, E value) {
-        if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("index: " + index);
-        }
-        // ListNode<E> current = nodeAt(index - 1);
-        // ListNode<E> newNode = new ListNode<E>(value, current.next, current);
-        // current.next = newNode;
-        // newNode.next.prev = newNode;
-        
+    //adds the value at the given index
+    //shifts subsequent values right
+    public abstract void add(int index, E value);
 
-        // ensureCapacity(size + 1);
-        // for (int i = size; i >= index + 1; i--) {
-        //     elementData[i] = elementData[i - 1];
-        // }
-        // elementData[index] = value;
-        // size++;
+    //adds the given list to the end of the current list
+    public abstract void addAll(List<E> other);
 
-    }
+    //removes the value at the given index
+    abstract public void remove(int index);
 
-    public void addAll(List<E> other) {
-        for (E value : other) {
-            add(value);
-        }
-    }
+    //replaces the current value at the index with the given value
+    abstract public void set(int index, E value);
 
-    public void remove(int index) {
+    //empties the list
+    public abstract void clear();
 
-    }
-
-    public void set(int index, E value) {
-
-    }
-
-    public void clear(){
-
-    }
-
-    public void ensureCapacity(int capacity) {
-        if (capacity > elementData.length) {
-            int newCapacity = elementData.length * 2 + 1;
-            if (capacity > newCapacity) {
-                newCapacity = capacity;
-            }
-            elementData = Arrays.copyOf(elementData, newCapacity);
-        }
-    }
+    //returns an iterator starting at the front of the list
+    public abstract Iterator<E> iterator();
 }
